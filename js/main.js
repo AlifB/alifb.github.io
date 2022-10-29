b381b270b52cb453ea434bf8e20c6b79f8b7c55b
Alif Borgmeier <alifalfath10@googlemail.com>
2022-10-29 19:39:40 +0200

    add contact info, content with animations and fontawesome

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
 const header = document.querySelector('header');
 const surname = document.querySelector('.surname');
+const copyrightYear = document.querySelector('#copyright-year');
 const myAge = document.querySelector('#my-age');
 const myBirthday = new Date('2000-03-02');
 const currentDate = new Date();
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            if(entry.target.classList.contains('contact-info')) {
+                entry.target.classList.add('show-contact');
+            }
+            if(entry.target.classList.contains('intro-snippet')) {
+                entry.target.classList.add('show-snippet');
+            }
+        } else {
+            entry.target.classList.remove('show-snippet');
+        }
+    });
+});
+
+const introSnippets = document.querySelectorAll('.intro-snippet');
+const contactInfo = document.querySelectorAll('.contact-info');
+introSnippets.forEach(snippet => observer.observe(snippet));
+contactInfo.forEach(contact => observer.observe(contact));
 
 let age = currentDate.getFullYear() - myBirthday.getFullYear();
 if(myBirthday.getMonth() > currentDate.getMonth() || (myBirthday.getMonth() == currentDate.getMonth() && myBirthday.getDate() > currentDate.getDate())) {
@@ -10,6 +31,7 @@ if(myBirthday.getMonth() > currentDate.getMonth() || (myBirthday.getMonth() == c
 }
 myAge.innerHTML = age;
 
+copyrightYear.innerHTML = currentDate.getFullYear();
 
 document.addEventListener('scroll', () => {
     if(!document.body.classList.contains('fixed-header') && (header.getBoundingClientRect().y <= 10)) {
