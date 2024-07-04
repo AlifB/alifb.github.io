98dc3f3cb29fb09f7825e3bd2eed234018b60e93
Alif Borgmeier <alifalfath10@googlemail.com>
2024-07-04 03:52:28 +0200

    added webpack

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+// Import CSS files
+import '../css/anim.css';
+import '../css/fonts.css';
+import '../css/style.css';
+
+// Import FontAwesome CSS files
+import '../assets/fontawesome/css/all.min.css';
+import '../assets/fontawesome/css/fontawesome.min.css';
+
+// Import JS files
+import './age.js';
+import './anim.js';
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js', // Adjust the entry point as needed
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'production', // 'production' for production builds
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|ttf|eot)$/i,
+                type: 'asset/resource', // Handle fonts
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader'], // Handle HTML files
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+};
